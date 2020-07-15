62f606cfe719fe0d041f25bb343f625005e1d8b0
nick8998 <19nipivanov98@gmail.com>
2020-07-15 17:32:59 +0300

    Созданы тесты для ответов и для вопросов

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_203539) do
+ActiveRecord::Schema.define(version: 2020_07_15_142510) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
