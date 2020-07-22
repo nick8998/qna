781ba2506d2118e70f35631991fadb29dbe7f929
nick8998 <19nipivanov98@gmail.com>
2020-07-22 15:37:23 +0300

    Проверка метода модели

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,15 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  
+  context "self.author_of?" do
+    let(:user) { create(:user) }
+    let!(:question) { create(:question, author: user) }
+    let!(:answer) { create(:answer, author: user, question: question) }
+    it 'checks author_id for question' do
+      expect(user.author_of?(question)).to eq(true)
+    end
+    it 'checks author_id for answer' do
+      expect(user.author_of?(answer)).to eq(true)
+    end
+  end
 end
