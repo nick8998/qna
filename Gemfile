fb24503b4030c73afbc978b4b71c07694c88e10c
nick8998 <19nipivanov98@gmail.com>
2020-08-05 15:56:09 +0300

    Добавлен s3@

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem "slim-rails"
 gem 'devise'
 
 gem 'jquery-rails'
+gem 'aws-sdk', '~> 3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("jquery")
 require("answer")
 require("question")
+require("direct_uploads")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
