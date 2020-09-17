3c03cd1699a5be16302a6104c5bc69659f6b09e9
nick8998 <19nipivanov98@gmail.com>
2020-09-17 14:26:02 +0300

    last1

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,13 +1,9 @@
 class Api::V1::BaseController < ApplicationController
   before_action :doorkeeper_authorize!
-  
-  def current_user
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
 
   private
 
-  def current_resource_owner
-    @current_resource_owner ||= current_user
+  def current_user
+    @current_useruser ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 end
--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
   authorize_resource :class => User
   def me
-    render json: current_resource_owner, serializer: ProfileSerializer
+    render json: current_user, serializer: ProfileSerializer
   end
   
   def other_profiles
-    @users = User.where.not(id: current_resource_owner)
+    @users = User.where.not(id: current_user)
     render json: @users, each_serializer: ProfileSerializer
   end
 end
